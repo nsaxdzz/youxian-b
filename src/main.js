9da6493d89fb95c8354d240428a10474f8912300
张大伟 <nsaxdzz@163.com>
2021-11-20 10:41:57 +0800

    系统布局

--- src/main.js
+++ src/main.js
@@ -2,7 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
+import './plugins/ant-design-vue.js'
+import '../src/assets/css/reset.less'
 Vue.config.productionTip = false
 
 new Vue({
